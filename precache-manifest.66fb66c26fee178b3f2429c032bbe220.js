7afe1ba4ca253e0df0cbd3503528ef40e5e9f7e5
krisshen <kris1126@163.com>
2018-10-07 13:21:24 +1300

    Updates

---
+++ precache-manifest.66fb66c26fee178b3f2429c032bbe220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f726a0ea515ef25c0f39",
+    "url": "/myreact/static/js/runtime~main.f726a0ea.js"
+  },
+  {
+    "revision": "ee7562deb64f5875befe",
+    "url": "/myreact/static/js/main.ee7562de.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/myreact/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "ee7562deb64f5875befe",
+    "url": "/myreact/static/css/main.71f634fa.chunk.css"
+  },
+  {
+    "revision": "675e91d773d8dd6969f3951a2288228a",
+    "url": "/myreact/index.html"
+  }
+];
